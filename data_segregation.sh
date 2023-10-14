eac7f600dbe40b59a8c4f842fa2cefc75a143a11
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-14 18:14:43 +0700

    fix mlflow run

--- data_segregation.sh
+++ data_segregation.sh
@@ -1,4 +1,4 @@
-mlflow run . -P input_artifact="sample-ml-pipeline/preprocessed_data.csv:latest" \
+mlflow run data_segregation -P input_artifact="sample-ml-pipeline/preprocessed_data.csv:latest" \
     -P artifact_root="data" \
     -P test_size=0.3 \
     -P stratify="genre"
