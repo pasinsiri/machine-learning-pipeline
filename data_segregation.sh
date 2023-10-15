dabc91d88ba45bbb28b26374688d8c6f9600c3bf
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-15 11:10:40 +0700

    add artifact_type

--- data_segregation.sh
+++ data_segregation.sh
@@ -1,4 +1,5 @@
 mlflow run data_segregation -P input_artifact="sample_ml_pipeline/preprocessed_data.csv:latest" \
     -P artifact_root="data" \
+    -P artifact_type="cleaned_data" \
     -P test_size=0.3 \
     -P stratify="genre"
